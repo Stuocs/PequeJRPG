933240583c50b2a8c7ba1a0aea177083a065de73
Stuocs <alfredo.espinber@gmail.com>
2024-11-30 01:47:53 +0100

    Pequeño juego de Rol por turnos hecho como ejemplo con python utilizando recursos de RPG Maker/Small RPG turn based game done as an example using RPG Maker resources

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/alfredo/Documentos/Cyber/Cyber/PPS/Pequeño_Juego_Rol/venv'
+set -gx VIRTUAL_ENV /home/alfredo/Documentos/Cyber/Cyber/PPS/git/master/PequeJRPG/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
